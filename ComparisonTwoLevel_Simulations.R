130d17ea9a3621f5d6ed284df63bea1ae2d42025
b-jorges <bjoern_joerges@hotmail.de>
2020-09-10 11:36:58 -0400

    j

---
+++ ComparisonTwoLevel_Simulations.R
@@ -0,0 +1,180 @@
+require(lme4)
+require(quickpsy)
+require(lmerTest)
+Where_Am_I <- function(path=T){
+  if (path == T){
+    dirname(rstudioapi::getSourceEditorContext()$path)
+  }
+  else {
+    rstudioapi::getSourceEditorContext()$path
+  }
+}
+
+setwd(Where_Am_I())
+
+source("SimulateDataFunction.r")
+
+SimulateDataframe_Twolevel = function(nParticipants,
+                                    ConditionOfInterest,
+                                    StandardValues,
+                                    reps,
+                                    PSE_Difference,
+                                    JND_Difference,
+                                    Multiplicator_PSE_Standard,
+                                    Multiplicator_SD_Standard,
+                                    SD_ResponseFunction,
+                                    Mean_Variability_Between,
+                                    SD_Variability_Between){
+  
+
+  Psychometric = SimulatePsychometricData(nParticipants,
+                                        ConditionOfInterest,
+                                        StandardValues,
+                                        reps,
+                                        PSE_Difference,
+                                        JND_Difference,
+                                        Multiplicator_PSE_Standard,
+                                        Multiplicator_SD_Standard,
+                                        Type_ResponseFunction,
+                                        SD_ResponseFunction,
+                                        Mean_Variability_Between,
+                                        SD_Variability_Between)
+   
+  Parameters = quickpsy(Psychometric,Difference,Answer,
+                              grouping = .(ID,ConditionOfInterest,StandardValues), 
+                              bootstrap = "none")$par
+  Parameters2 = Parameters %>%
+  filter(parn == "p1") %>%
+  select(ID,ConditionOfInterest,Mean=par, StandardValues)
+  Parameters2$SD = Parameters$par[Parameters$parn == "p2"]
+  FittedPsychometricFunctions = Parameters2
+   
+  ANOVA_Mean = lm(Mean ~ as.factor(ConditionOfInterest)*as.factor(StandardValues),FittedPsychometricFunctions)
+  Pvalue_Mean_ANOVA = summary(ANOVA_Mean)$coefficients[14]
+  ANOVA_SD = lm(SD ~ as.factor(ConditionOfInterest)*as.factor(StandardValues),FittedPsychometricFunctions)
+  Pvalue_SD_ANOVA = summary(ANOVA_SD)$coefficients[14]
+
+  GLMM = glmer(Answer ~ Difference*ConditionOfInterest + (Difference + ConditionOfInterest |ID) + (Difference|StandardValues),
+             family = binomial(link = "logit"),
+             data = Psychometric,
+             nAGQ = 1,
+             glmerControl(optimizer = "nloptwrap"))
+
+  TwoLevelMean = lmer(Mean ~ ConditionOfInterest + (1|ID) + (1|StandardValues),
+                    data = FittedPsychometricFunctions)
+
+  TwoLevelSD = lmer(SD ~ ConditionOfInterest + (1|ID) + (1|StandardValues),
+                    data = FittedPsychometricFunctions)
+
+  c(summary(GLMM)$coef[15],
+  summary(GLMM)$coef[16],
+  summary(TwoLevelMean)$coef[10],
+  summary(TwoLevelSD)$coef[10],
+  Pvalue_Mean_ANOVA,
+  Pvalue_SD_ANOVA)
+}
+
+#######Comparison of Two Level approach and GLMM approach
+ConditionOfInterest = c(0,1)
+StandardValues = c(5,8)
+Range_reps = c(40,70,100)
+Range_PSE_Difference = c(-0.05,-0.025,0,0.025,0.05)
+Range_JND_Difference = c(-0.1,-0.05,0,0.05,0.1)
+Multiplicator_PSE_Standard = 0
+Multiplicator_SD_Standard = 0.108
+SD_ResponseFunction = 0.1
+Mean_Variability_Between = 0.2
+SD_Variability_Between = 0.2
+nIterations = 100
+Range_Participants = c(10,12,14,16,18,20)
+
+TotalNumber = length(Range_reps)*length(Range_PSE_Difference)*length(Range_JND_Difference)*length(Range_Participants)
+CurrentRunthrough = 0
+rightnow = Sys.time()
+
+for (reps in Range_reps){
+  for (PSE_Difference in Range_PSE_Difference){
+    for (JND_Difference in Range_JND_Difference){
+      for (n in Range_Participants){
+        
+        TimeStartTrial = Sys.time()
+        
+        Pvalues_Accuracy = c()
+        Pvalues_Precision = c()
+        Pvalues_Accuracy_TwoLevel = c()
+        Pvalues_Precision_TwoLevel = c()
+        Pvalues_Accuracy_ANOVA = c()
+        Pvalues_Precision_ANOVA = c()
+        
+        for (j in 1:nIterations){
+        Pvalues = SimulateDataframe_Twolevel(n, 
+                                               ConditionOfInterest, 
+                                               StandardValues, 
+                                               reps, 
+                                               PSE_Difference, 
+                                               JND_Difference, 
+                                               Multiplicator_PSE_Standard, 
+                                               Multiplicator_SD_Standard, 
+                                               SD_ResponseFunction, 
+                                               Mean_Variability_Between, 
+                                               SD_Variability_Between)
+          
+                Pvalues_Accuracy = c(Pvalues_Accuracy, Pvalues[1])
+                Pvalues_Precision = c(Pvalues_Precision, Pvalues[2])
+                Pvalues_Accuracy_TwoLevel = c(Pvalues_Accuracy_TwoLevel, Pvalues[3])
+                Pvalues_Precision_TwoLevel = c(Pvalues_Precision_TwoLevel, Pvalues[4])
+                Pvalues_Accuracy_ANOVA = c(Pvalues_Accuracy_ANOVA, Pvalues[5])
+                Pvalues_Precision_ANOVA = c(Pvalues_Precision_ANOVA, Pvalues[6])
+              }
+              
+          CurrentRunthrough = CurrentRunthrough + 1
+              
+          PowerfulDataframe = data.frame(n=n, 
+                                          ConditionsOfInterest=length(ConditionOfInterest), 
+                                          StandardValue1=StandardValues[1],
+                                          StandardValue2=StandardValues[2], 
+                                          reps=reps, 
+                                          PSE_Difference=PSE_Difference, 
+                                          JND_Difference=JND_Difference, 
+                                          Multiplicator_PSE_Standard=Multiplicator_PSE_Standard, 
+                                          Multiplicator_SD_Standard=Multiplicator_SD_Standard, 
+                                          SD_ResponseFunction=SD_ResponseFunction, 
+                                          Mean_Variability_Between=Mean_Variability_Between, 
+                                          SD_Variability_Between=SD_Variability_Between, 
+                                          power_Accuracy = mean(Pvalues_Accuracy < 0.05),  
+                                          power_Precision = mean(Pvalues_Precision < 0.05),
+                                          power_Accuracy_Twolevel = mean(Pvalues_Accuracy_TwoLevel < 0.05),  
+                                          power_Precision_Twolevel = mean(Pvalues_Precision_TwoLevel < 0.05),
+                                          power_Accuracy_ANOVA = mean(Pvalues_Accuracy_ANOVA < 0.05),  
+                                          power_Precision_ANOVA = mean(Pvalues_Precision_ANOVA < 0.05),
+                                          Duration = Sys.time() - TimeStartTrial)
+          if (CurrentRunthrough == 1){
+              
+            write.table(PowerfulDataframe, file = "PowerTwoLevelComparison.csv", sep = ",", row.names = FALSE)} 
+          
+          if (CurrentRunthrough > 1){
+            write.table(PowerfulDataframe, file = "PowerTwoLevelComparison.csv", 
+                        sep = ",", append = TRUE, quote = FALSE, col.names = FALSE, row.names = FALSE)}
+          
+          print(paste0("RUNTHROUGH ", 
+                       CurrentRunthrough, 
+                       "out of ", 
+                       TotalNumber,
+                       ": ", 
+                       n, 
+                       ", reps, ", 
+                       PSE_Difference, 
+                       ", ", 
+                       JND_Difference, 
+                       ", " , 
+                       mean(Pvalues_Accuracy < 0.05), 
+                       ", " , 
+                       mean(Pvalues_Precision < 0.05),
+                       " ", 
+                       Sys.time() - TimeStartTrial, 
+                       " END. "))
+          
+      }
+    }
+  }
+}
---
+++ ComparisonTwolevel_Plots.R
@@ -0,0 +1,91 @@
+###Pull the whole repository
+require(dplyr)
+require(ggplot2)
+require(cowplot)
+theme_set(theme_cowplot())
+
+
+Where_Am_I <- function(path=T){
+  if (path == T){
+    dirname(rstudioapi::getSourceEditorContext()$path)
+  }
+  else {
+    rstudioapi::getSourceEditorContext()$path
+  }
+}
+
+setwd(Where_Am_I())
+
+
+########################################################################
+##############compare power for GLMM and Two-Level approach#############
+########################################################################
+Dataframe_wide_Big = rbind(read.csv(header = T, file = paste0(Where_Am_I(),"/Data/Powers_BiggerModel_2020_09_10.csv"))) %>%
+  select(power_Accuracy,power_Precision,power_Accuracy_Twolevel,power_Precision_Twolevel,power_Accuracy_ANOVA,
+         power_Precision_ANOVA,n, PSE_Difference, JND_Difference, reps)
+Dataframe_Powers_Big = data.frame(PSE_Difference = rep(Dataframe_wide_Big$PSE_Difference,4),
+                                  JND_Difference = rep(Dataframe_wide_Big$JND_Difference,4),
+#                                  PSE_Difference = rep(Dataframe_wide_Big$PSE_Difference,6),
+#                                  JND_Difference = rep(Dataframe_wide_Big$JND_Difference,6),
+                                  n = rep(Dataframe_wide_Big$n,4),
+                                  reps = rep(Dataframe_wide_Big$reps,4),
+#                                 n = rep(Dataframe_wide_Big$n,6),
+#                                  reps = rep(Dataframe_wide_Big$reps,6),
+                                  power = c(Dataframe_wide_Big$power_Accuracy,
+                                            Dataframe_wide_Big$power_Precision,
+                                            Dataframe_wide_Big$power_Accuracy_Twolevel,
+                                            Dataframe_wide_Big$power_Precision_Twolevel),
+#                                            Dataframe_wide_Big$power_Accuracy_ANOVA,
+#                                            Dataframe_wide_Big$power_Precision_ANOVA),
+                                  label = c(rep("Accuracy_GLMM",length(Dataframe_wide_Big$reps)),
+                                            rep("Precision_GLMM",length(Dataframe_wide_Big$reps)),
+                                            rep("Accuracy_Twolevel",length(Dataframe_wide_Big$reps)),
+                                            rep("Precision_Twolevel",length(Dataframe_wide_Big$reps))))
+#                                            rep("Accuracy_Twolevel_ANOVA",length(Dataframe_wide_Big$reps)),
+#                                            rep("Precision_Twolevel_ANOVA",length(Dataframe_wide_Big$reps))))
+
+
+Powers1 = ggplot(Dataframe_Powers_Big %>% filter(reps == 40), aes(n,power,color = label)) +
+  geom_line(size = 2) +
+  facet_grid(JND_Difference~PSE_Difference) +
+  xlab("N° of Participants") +
+  ylab("Power") +
+  geom_hline(linetype = 2, yintercept = 0.8) +
+  geom_hline(linetype = 1, yintercept = 0.05) +
+  geom_hline(linetype = 3, yintercept = 0.95) +
+  scale_x_continuous(breaks=c(10,15,20)) +
+  scale_y_continuous(breaks=c(0.25,0.75)) +
+  scale_color_manual(values = c(BlauUB,LightBlauUB,Red,LightRed), 
+                     name = "") +
+  ggtitle("A. 40 Repetitions")
+
+Powers2 = ggplot(Dataframe_Powers_Big %>% filter(reps == 70), aes(n,power,color = label)) +
+  geom_line(size = 2) +
+  facet_grid(JND_Difference~PSE_Difference) +
+  xlab("N° of Participants") +
+  ylab("Power") +
+  geom_hline(linetype = 2, yintercept = 0.8) +
+  geom_hline(linetype = 1, yintercept = 0.05) +
+  geom_hline(linetype = 3, yintercept = 0.95) +
+  scale_x_continuous(breaks=c(10,15,20)) +
+  scale_y_continuous(breaks=c(0.25,0.75)) +
+  scale_color_manual(values = c(BlauUB,LightBlauUB,Red,LightRed), 
+                     name = "") +
+  ggtitle("A. 70 Repetitions")
+
+Powers3 = ggplot(Dataframe_Powers_Big %>% filter(reps == 100), aes(n,power,color = label)) +
+  geom_line(size = 2) +
+  facet_grid(JND_Difference~PSE_Difference) +
+  xlab("N° of Participants") +
+  ylab("Power") +
+  geom_hline(linetype = 2, yintercept = 0.8) +
+  geom_hline(linetype = 1, yintercept = 0.05) +
+  geom_hline(linetype = 3, yintercept = 0.95) +
+  scale_x_continuous(breaks=c(10,15,20)) +
+  scale_y_continuous(breaks=c(0.25,0.75)) +
+  scale_color_manual(values = c(BlauUB,LightBlauUB,Red,LightRed), 
+                     name = "") +
+  ggtitle("A. 100 Repetitions")
+
+plot_shared_legend(Powers1,Powers2, Powers3)
+ggsave("Figures/PowersBigModel.jpg", w = 12, h = 8)
